2ee108fde9e169d034cd1ff1eb867724a6976c32
Yi <yiz.codes@gmail.com>
2025-11-09 12:04:00 -0500

    noice/notification tweak

--- init.lua
+++ init.lua
@@ -937,7 +937,7 @@ require('lazy').setup({
       -- require("mini.jump").setup()
       require('mini.tabline').setup()
       require('mini.starter').setup()
-      require('mini.notify').setup()
+      -- require('mini.notify').setup()
       require('mini.move').setup()
       require('mini.indentscope').setup()
 
@@ -1017,11 +1017,11 @@ require('lazy').setup({
         },
         -- you can enable a preset for easier configuration
         presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
+          bottom_search = false, -- use a classic bottom cmdline for search
           command_palette = true, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
+          inc_rename = true, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true, -- add a border to hover docs and signature help
         },
       }
     end,
