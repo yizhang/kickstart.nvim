d958931219024239d4b42a6730c71dbf62b2c9ef
Yi <yiz.codes@gmail.com>
2025-11-14 16:19:33 -0500

    add zig lang server zls

--- init.lua
+++ init.lua
@@ -679,6 +679,7 @@ require('lazy').setup({
         -- gopls = {},
         pyright = {},
         rust_analyzer = {},
+        zls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
