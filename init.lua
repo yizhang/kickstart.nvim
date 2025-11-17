8fe4db421a7fcb2022489fcb7738fa32ba7057f1
Yi <yiz.codes@gmail.com>
2025-11-16 20:57:12 -0500

    remove gitsign customization

--- init.lua
+++ init.lua
@@ -274,15 +274,6 @@ require('lazy').setup({
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
