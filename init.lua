a115a0ff91931de68882c6c8d807e4b6441427bb
Yi <yiz.codes@gmail.com>
2025-11-27 11:18:43 -0500

    don't show documentation or signature help

--- init.lua
+++ init.lua
@@ -745,7 +745,7 @@ require('lazy').setup({
       completion = {
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        -- documentation = { auto_show = false, auto_show_delay_ms = 500 },
       },
 
       sources = {
@@ -767,7 +767,7 @@ require('lazy').setup({
       fuzzy = { implementation = 'lua' },
 
       -- Shows a signature help window while you type arguments for a function
-      signature = { enabled = true },
+      signature = { enabled = false },
     },
   },
 
