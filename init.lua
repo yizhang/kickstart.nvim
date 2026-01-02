a0f7b3a135a36518b476cf6b7956956590193199
Yi <yiz.codes@gmail.com>
2026-01-02 17:10:35 -0500

    mini surround update

--- init.lua
+++ init.lua
@@ -816,7 +816,17 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'gsa', -- Add surrounding in Normal and Visual modes
+          delete = 'gsd', -- Delete surrounding
+          find = 'gsf', -- Find surrounding (to the right)
+          find_left = 'gsF', -- Find surrounding (to the left)
+          highlight = 'gsh', -- Highlight surrounding
+          replace = 'gsr', -- Replace surrounding
+          update_n_lines = 'gsn', -- Update `n_lines`
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
@@ -836,7 +846,7 @@ require('lazy').setup({
       -- require('mini.pick').setup()
       -- require('mini.extra').setup()
       -- require("mini.jump").setup()
-      require('mini.tabline').setup()
+      -- require('mini.tabline').setup()
       require('mini.starter').setup()
       -- require('mini.notify').setup()
       require('mini.move').setup()
