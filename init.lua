e5bfb5dfcaa831d40b20378eae373a1f41c1233d
Yi <yiz.codes@gmail.com>
2026-01-02 17:10:01 -0500

    yank using systemplus register

--- init.lua
+++ init.lua
@@ -114,9 +114,9 @@ vim.o.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
+-- vim.schedule(function()
+--   vim.o.clipboard = 'unnamedplus'
+-- end)
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -168,6 +168,14 @@ vim.o.confirm = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+
+-- If yanking without specifying a register, then use the system clipboard register by default.
+vim.keymap.set({ 'n', 'x' }, 'y', function()
+  if vim.v.register == '"' then
+    return '"+y'
+  end
+  return 'y'
+end, { expr = true })
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
